6e7976a63dd6f6db071e6c55f7da8dcb61db41c7
Nameera Pathan <66337671+dedsecwrench@users.noreply.github.com>
2023-01-10 17:06:01 +0530

    added ninjascroll and scroll on click of link

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,64 @@
 import gsap from "gsap";
-// import Scrollbar from "smooth-scrollbar";
+// import Scrollbar,{ ScrollbarPlugin } from 'smooth-scrollbar';
+
+
+// class AnchorPlugin extends ScrollbarPlugin {
+//     static pluginName = 'anchor';
+  
+//     onHashChange = () => {
+//       this.jumpToHash(window.location.hash);
+//     };
+  
+//     onClick = (event) => {
+//       const { target } = event;
+  
+//       if (target.tagName !== 'A') {
+//         return;
+//       }
+  
+//       const hash = target.getAttribute('href');
+  
+//       if (!hash || hash.charAt(0) !== '#') {
+//         return;
+//       }
+  
+//       this.jumpToHash(hash);
+//     };
+  
+//     jumpToHash = (hash) => {
+//       const { scrollbar } = this;
+  
+//       if (!hash) {
+//         return;
+//       }    
+  
+//       // reset scrollTop
+//       scrollbar.containerEl.scrollTop = 0;
+  
+//       scrollbar.scrollIntoView(document.querySelector(hash));
+//     };
+  
+//     onInit() {
+//       this.jumpToHash(window.location.hash);
+  
+//       window.addEventListener('hashchange', this.onHashChange);
+  
+//       this.scrollbar.contentEl.addEventListener('click', this.onClick);
+//     }
+  
+//     onDestory() {
+//       window.removeEventListener('hashchange', this.onHashChange);
+  
+//       this.scrollbar.contentEl.removeEventListener('click', this.onClick);
+//     }
+//   }
+  
+//   // usage
+//   Scrollbar.use(AnchorPlugin);
+
+// //   SCROLLING ANCHOR
+
+
 const bar = document.querySelector(".loading__bar--inner");
 const counter_num = document.querySelector(".loading__counter--number");
 // const circle = document.querySelector(".loading__box");
@@ -40,11 +99,12 @@ let barInterval = setInterval(()=>{
         delay:1,
         bottom:"4rem",
     })
-    // let pageSmoothScroll = Scrollbar.init(document.body);
+    // let options={
+    //     damping:0.1,
+    //     // alwaysShowTracks:true
+    // }
+
    } 
 },20)
 
-// const btn = document.querySelector("#btn");
-// btn.addEventListener('click',()=>{
-// console.log('clicked');
-// })
+// Scrollbar.init(document.body);
