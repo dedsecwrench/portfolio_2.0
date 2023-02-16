a7416cda950bb6418d4e8dcb62c8f6383a4e58a3
Nameera Pathan <66337671+dedsecwrench@users.noreply.github.com>
2023-02-16 21:00:06 +0530

    removed main.js

--- src/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-const toggleBtn = document.querySelector('.header__toggle-btn');
-const navbarContent = document.querySelector('.header__left');
-console.log(navbarContent);
-
-toggleBtn.addEventListener('click', () => {
-  navbarContent.classList.toggle('collapse');
-});
