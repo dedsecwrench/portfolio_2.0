e82950382c970fa80877e42601513f6b2ede7dc8
Nameera Pathan <66337671+dedsecwrench@users.noreply.github.com>
2025-03-07 21:51:16 +0530

    v-3 sphere radius change dynamically

--- src/assets/js/sphere.js
+++ src/assets/js/sphere.js
@@ -1,4 +1,4 @@
-    const Texts = [
+    const Skills = [
         'HTML5', 'CSS3', 'JavaScript',
         'Mongoose', 'React JS',
         'Python', 'SASS', 'SQL',
@@ -6,10 +6,11 @@
         'Express JS', 'GSAP' , 'MYSQL', 'Bootstrap', 'npm' , 'Tailwind CSS', 'Github'
     ];
 
-    var tagCloud = TagCloud('.Sphere', Texts, {
+    var tagCloud = TagCloud('.Sphere', Skills, {
 
         // Sphere radius in px
-        radius: 227,
+        // radius: 227,
+        radius: window.innerWidth < 700 ? 205 : 230,
 
         // animation speed
         // slow, normal, fast
@@ -20,10 +21,13 @@
         direction: 135,
 
         // interaction with mouse or not [Default true (decelerate to rolling init speed, and keep rolling with mouse).]
-        keep: true
+        keep:window.innerWidth < 700 ? false : true
 
     });
 
+
+    // console.log('width here: ', window.innerWidth)
+    // console.log('height here: ', window.innerHeight)
     // Giving color to each text in sphere
     // var color = 'rgb(17, 238, 238)';
     // let color = '#88f7f7';
